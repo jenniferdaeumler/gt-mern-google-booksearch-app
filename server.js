53debb695d0f1ca706f894ce6321e34ed80bf9c7
Jennifer Daeumler <jenniferdaeumler@gmail.com>
2020-08-08 15:00:42 -0500

    Added rest from MERn starter

--- server.js
+++ server.js
@@ -1,12 +1,17 @@
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
+const path = require ("path"); 
 
 const PORT = process.env.PORT || 3001;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+app.use(express.static("client/build"));
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "./client/build/index.html"));
+});
 app.get("/api/config", (req, res) => {
   res.json({ success: true });
 });
