0bd6d78b1978c0246e9bc8e7ba8648938084b733
Jennifer Daeumler <jenniferdaeumler@gmail.com>
2020-08-08 16:06:07 -0500

    style search

--- client/src/containers/Search/Search.jsx
+++ client/src/containers/Search/Search.jsx
@@ -1,13 +1,34 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
+import "./Search.css";
 
 class Search extends Component {
-    render() {
-        return (
-            <div>
-                <h1>Search books</h1>
-            </div>
-        );
-    }
+  render() {
+    return (
+      <div>
+          <div className="container">
+              <div className="row">
+                  <div className = "col-12">
+        <form>
+          <div className="form-group">
+            <label for="exampleInputEmail1" id="searchText">Search for a book...</label>
+            <input
+              type="email"
+              class="form-control"
+              id="searchForm"
+              aria-describedby="emailHelp"
+              placeholder="Search by title or author."
+            />
+            <button type="submit" class="btn btn-primary" id="searchButton">
+              Search
+            </button>
+          </div>
+        </form>
+        </div>
+        </div>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default Search;
