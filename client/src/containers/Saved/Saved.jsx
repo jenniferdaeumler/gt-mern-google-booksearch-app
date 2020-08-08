d489a9d74e7ea96382486fd5cad7c7c78e2c0786
Jennifer Daeumler <jenniferdaeumler@gmail.com>
2020-08-08 16:14:24 -0500

    Saved books card created. Hard coded now

--- client/src/containers/Saved/Saved.jsx
+++ client/src/containers/Saved/Saved.jsx
@@ -1,13 +1,31 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
+import "./Saved.css";
 
 class Saved extends Component {
-    render() {
-        return (
-            <div>
-                <h1>Saved Books</h1>
+  render() {
+    return (
+      <div>
+        <div className="container">
+          <div className="row">
+            <div className="col-12">
+              <h1 id="savedBooksText">Saved Books</h1>
+              <div className="card">
+                <div className="card-body">
+                  <p>
+                    Picture: 
+                    Title:
+                    Author(s):
+                    Description:
+                    Link:
+                  </p>
+                </div>
+              </div>
             </div>
-        );
-    }
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default Saved;
