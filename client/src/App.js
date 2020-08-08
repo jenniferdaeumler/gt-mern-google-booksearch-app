9c1eaf18fae0ee6a7f08fb10e69fb1f6316f7172
Jennifer Daeumler <jenniferdaeumler@gmail.com>
2020-08-08 13:30:57 -0500

    React router installed

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,18 @@
-import React from 'react';
+import React, {useEffect} from 'react';
+import axios from "axios";
+import {BrowserRouter as Router, Switch, Route} from "react-router-dom";
 
 function App() {
+  useEffect(()=>{
+    axios.get("/api/config")
+    .then((response)=>{
+      console.log(response.data);
+    })
+    .catch((err)=>{
+      console.log(err);
+    })
+  }, [])
+ 
   return (
     <div>
       <h1>Hello World.</h1>
