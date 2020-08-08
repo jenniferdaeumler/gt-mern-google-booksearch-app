7aa56fea8075c77f90695f7b733bfd83b4201f19
Jennifer Daeumler <jenniferdaeumler@gmail.com>
2020-08-08 15:39:23 -0500

    Homepmage

--- client/src/App.js
+++ client/src/App.js
@@ -1,33 +1,34 @@
-import React, {useEffect} from 'react';
+import React, { useEffect } from "react";
 import axios from "axios";
-import {BrowserRouter as Router, Switch, Route} from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Home from "./containers/Home/Home";
 import NoMatch from "./containers/NoMatch/NoMatch";
-import Search from './containers/Search/Search';
-import Saved from './containers/Saved/Saved';
-import Nav from './containers/Nav/Nav';
+import Search from "./containers/Search/Search";
+import Saved from "./containers/Saved/Saved";
+import Nav from "./containers/Nav/Nav";
 
 function App() {
-  useEffect(()=>{
-    axios.get("/api/config")
-    .then((response)=>{
-      console.log(response.data);
-    })
-    .catch((err)=>{
-      console.log(err);
-    })
-  }, [])
- 
+  useEffect(() => {
+    axios
+      .get("/api/config")
+      .then((response) => {
+        console.log(response.data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []);
+
   return (
-<Router>
-  <Switch>
-    <Nav/>
-    <Route exact path="/" component={Home}/>
-    <Route exact path="/search" component={Search}/>
-    <Route exact path="/saved" component={Saved}/> 
-    <Route component = {NoMatch}/>
-  </Switch>
-</Router>
+    <Router>
+      <Nav />
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route exact path="/search" component={Search} />
+        <Route exact path="/saved" component={Saved} />
+        <Route component={NoMatch} />
+      </Switch>
+    </Router>
   );
 }
 
--- client/src/containers/Home/Home.jsx
+++ client/src/containers/Home/Home.jsx
@@ -1,13 +1,21 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
+import './Home.css';
 
 class Home extends Component {
-    render() {
-        return (
-            <div>
-                <h1>Homepage</h1>
-            </div>
-        );
-    }
+  render() {
+    return (
+      <div>
+        <div className="jumbotron jumbotron-fluid" id="homepageJumbotron">
+          <div className="container">
+            <h1 className="display-4"id="homeText">Google Books</h1>
+            <p className="lead" id="homeTextSmall">
+              Search for, view, and save books you want to read.
+            </p>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default Home;
